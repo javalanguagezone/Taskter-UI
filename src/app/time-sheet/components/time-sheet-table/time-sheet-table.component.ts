97e8735d7a31298b44ead5154a3aa3836d8ad38c
Nermin <nermin.milisic@edu.fit.ba>
2019-02-27 16:20:20 +0100

    fixed timesheet table not updating automatically when adding/editing entries

--- src/app/time-sheet/components/time-sheet-table/time-sheet-table.component.ts
+++ src/app/time-sheet/components/time-sheet-table/time-sheet-table.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ProjectTaskEntry } from '../../../shared/models/projectTaskEntry.model';
 import { TimeEntryDialogueComponent } from '../time-entry-dialogue/time-entry-dialogue.component';
 import { MatDialog } from '@angular/material';
+import * as moment from 'moment';
+import { TimesheetService } from '../../services/timesheet.service';
 
 @Component({
   selector: 'tsk-time-sheet-table',
@@ -12,9 +14,11 @@ import { MatDialog } from '@angular/material';
 export class TimeSheetTableComponent implements OnInit {
 
   @Input() tasks: ProjectTaskEntry[];
+  @Input() date: moment.Moment;
   displayedColumns: string[] = ['Project Description', 'Duration', 'Action'];
 
-  constructor(private dialogue: MatDialog) { }
+  constructor(private dialogue: MatDialog, private timeSheetServices: TimesheetService,
+    ) { }
 
   ngOnInit() {
   }
@@ -23,12 +27,21 @@ export class TimeSheetTableComponent implements OnInit {
     return this.tasks.map(t => t.durationInMin).reduce(( acc, value ) => acc + value, 0);
   }
 
+  getTaskByDate(year, month, day): void {
+    this.timeSheetServices.getTasks(year, month, day)
+      .subscribe(task => {
+        this.tasks = task; }
+      );
+  }
 
   openDialog(id: number): void {
     const dialogueRef = this.dialogue.open(TimeEntryDialogueComponent, {
       width: '350px',
-      data: {date: null, EntryId: id}
+      data: {date: this.date, EntryId: id}
     });
 
+    dialogueRef.afterClosed().subscribe(result => {
+      this.getTaskByDate(this.date.year(), this.date.month() + 1, this.date.date());
+    });
   }
 }
